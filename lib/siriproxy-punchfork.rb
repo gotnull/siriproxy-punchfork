786c11da16d35d31d9794c57c71b2dc1a5306c01
Fulvio Cusumano <fcusumano@gmail.com>
2012-01-04 18:32:28 +1100

    Update lib/siriproxy-punchfork.rb

--- lib/siriproxy-punchfork.rb
+++ lib/siriproxy-punchfork.rb
@@ -6,7 +6,7 @@ require 'httparty'
 class SiriProxy::Plugin::PunchFork < SiriProxy::Plugin
 
 	def initialize(config = {})
-		apikey = "<apikey>"
+		apikey = "1234567890"
 		@uri = "http://api.punchfork.com/random_recipe?key=#{apikey}"
 		@responses = [  "Why don't you cook some fucking",
 				"Go to the supermarket and cook some fucking",
@@ -22,26 +22,28 @@ class SiriProxy::Plugin::PunchFork < SiriProxy::Plugin
 
 		#puts recipe
 
+		r = ""
+		
 		unless recipe.nil?
 			if (recipe['error'])
-				puts "How the fuck should I know?"
+				r = "How the fuck should I know?"
 			else
-				puts "#{@responses[rand(@responses.size)]} #{recipe['recipe']['title']}."
+				r = "#{@responses[rand(@responses.size)]} #{recipe['recipe']['title']}."
 			end
 		end
+		
+		return r
 
 	end
 
 	# Example: "Siri, what's for dinner?", "Siri, what's to eat?"
 	listen_for /(for|to) (dinner|eat)/i do |item|
 
-		say @responses[rand(@responses.size)]
+		say "What's for dinner?"
 
 		Thread.new {
 		
-			recipe = random_recipe
-			
-			say recipe
+			say "#{random_recipe}"
 		
 			request_completed
 		}
