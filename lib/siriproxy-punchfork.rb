e61e618f30eb3d4841d70a079d0f495d98b421a3
Fulvio Cusumano <fcusumano@gmail.com>
2012-01-04 16:27:56 +1100

    first commit

---
+++ lib/siriproxy-punchfork.rb
@@ -0,0 +1,47 @@
+require 'cora'
+require 'siri_objects'
+require 'json'
+require 'httparty'
+
+class SiriProxy::Plugin::PunchFork < SiriProxy::Plugin
+
+	def initialize(config = {})
+		apikey = "690d6c63c08a79"
+		@uri = "http://api.punchfork.com/random_recipe?key=#{apikey}"
+		@responses = [  "Why don't you cook some fucking",
+						"Go to the supermarket and cook some fucking",
+						"You have cool dreadlocks, but why don't you cook some fucking",
+						"That's easy, just cook some fucking"
+		]
+	end
+
+	def random_recipe
+
+		page = HTTParty.get(@uri).body rescue nil
+		recipe = JSON.parse(page) rescue nil
+
+		#puts recipe
+
+		unless recipe.nil?
+			return "#{@responses[rand(@responses.size)]} #{recipe['recipe']['title']}."
+		end
+
+	end
+
+	# Example: "Siri, what's for dinner?"
+	listen_for /(for|to) (dinner|eat)/i do |item|
+
+		say @responses[rand(@responses.size)]
+
+		Thread.new {
+		
+			recipe = random_recipe
+			
+			say recipe
+		
+			request_completed
+		}
+
+	end
+
+end
---
+++ siriproxy-punchfork.gemspec
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "siriproxy-punchfork"
+  s.version     = "0.0.1" 
+  s.authors     = ["rakusu"]
+  s.email       = [""]
+  s.homepage    = ""
+  s.summary     = %q{Siri controlled random recipe PunchFork generator}
+  s.description = %q{Ask Siri what's for dinner and she'll return a random recipe from PunchFork}
+
+  s.rubyforge_project = "siriproxy-punchfork"
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  #s.add_runtime_dependency "nokogiri"
+end
+
